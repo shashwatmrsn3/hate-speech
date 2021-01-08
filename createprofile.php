3e141b6b54d0a1e575045e96e2fdbab36bd8998e
LAPTOP-EP3GFEIM\ashok <ashok.sharma3795@gmail.com>
2021-01-08 13:25:15 +0545

    anotherr text

--- createprofile.php
+++ createprofile.php
@@ -16,8 +16,7 @@
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
       integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-      crossorigin="anonymous"
-    />
+      crossorigin="anonymous"/>
 
     <link rel="stylesheet" href="css/style.css"/>
     <title>Welcome To The Developer Connector</title>
