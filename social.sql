9b059b13238c9c41158c9b36075d31c03f0cf8d8
rupa.chaulagain@hotmail.com <rupa.chaulagain@yahoo.com>
2021-04-08 20:31:24 +0545

    Updated sql file to include all three tables

--- social.sql
+++ social.sql
@@ -109,3 +109,79 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2020 at 03:49 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `social`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userimages`
+--
+
+CREATE TABLE `userimages` (
+  `Id` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `ImageName` varchar(200) NOT NULL,
+  `ImageContent` varchar(200) NOT NULL,
+  `ImageContentType` varchar(200) NOT NULL,
+  `ImageContentLocation` varchar(200) NOT NULL,
+  `IsProfileImage` bit(1) NOT NULL DEFAULT b'0',
+  `CreatedDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `userimages`
+--
+
+INSERT INTO `userimages` (`Id`, `UserId`, `ImageName`, `ImageContent`, `ImageContentType`, `ImageContentLocation`, `IsProfileImage`, `CreatedDate`) VALUES
+(7, 19, 'anakin.jpg', 'images/anakin.jpg', 'jpg', 'images/anakin.jpg', b'1', '0000-00-00 00:00:00'),
+(8, 20, 'ok.jpg', 'images/ok.jpg', 'jpg', 'images/ok.jpg', b'1', '0000-00-00 00:00:00'),
+(9, 21, 'rickit.jpg', 'images/rickit.jpg', 'jpg', 'images/rickit.jpg', b'1', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userimages`
+--
+ALTER TABLE `userimages`
+  ADD PRIMARY KEY (`Id`,`IsProfileImage`),
+  ADD UNIQUE KEY `FK_UserImages_ON_UserId` (`UserId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userimages`
+--
+ALTER TABLE `userimages`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
